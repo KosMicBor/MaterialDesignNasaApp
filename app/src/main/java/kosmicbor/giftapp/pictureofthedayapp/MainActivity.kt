1b3d2527cb9b938f0ddec6c751fd64e07aaa22cb
KosMicBor <gaijin-dk@mail.ru>
2022-02-01 20:20:19 +0500

    init project

---
+++ app/src/main/java/kosmicbor/giftapp/pictureofthedayapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package kosmicbor.giftapp.pictureofthedayapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kosmicbor.giftapp.pictureofthedayapp.ui.main.PictureOfTheDayFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, PictureOfTheDayFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
